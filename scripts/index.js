5b1a409549d12162ce02fecd8458e5a1284c69d5
Qwaniii <115154547+Qwaniii@users.noreply.github.com>
2023-01-18 18:18:27 +0300

    add popup

--- scripts/index.js
+++ scripts/index.js
@@ -11,3 +11,20 @@ for (i = 0; i < cards.length; i++) {
     const width = cards[i].offsetWidth;
     cards[i].style.height = width * 0.6 + "px";
 }
+
+let addBtn = document.querySelector("#add");
+let popupForm = document.querySelector("#popup-form");
+let closePopupForm = document.querySelector(".popup-close");
+
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!popupForm.classList.contains("acrive")) {
+        popupForm.classList.add("active");
+        popupForm.parentElement.classList.add("active");
+    }
+});
+
+closePopupForm.addEventListener("click", () => {
+    popupForm.classList.remove("active");
+    popupForm.parentElement.classList.remove("active");
+});
