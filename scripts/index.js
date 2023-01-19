dfb54cdf5c023cb8229925ecb736bf3f77118d2d
Qwaniii <115154547+Qwaniii@users.noreply.github.com>
2023-01-19 23:01:42 +0300

    some code

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@
 
 let main = document.querySelector("main");
 
+const api = new Api("qwaniii");
+
 // cats.forEach(function(cat) {
 //     let card = `<div class="${cat.favourite ? "card like" : "card"}" style="background-image: url(${cat.img_link})">
 //         <span>${cat.name}</span>
@@ -9,11 +11,11 @@ let main = document.querySelector("main");
 //     main.innerHTML += card 
 // });
 
-const updCards = function(data) {
+const updCards = function (data) {
     main.innerHTML = "";
-    data.forEach(function(cat) {
-        if(cat.id) {
-            let card = `<div class="${cat.favourite ? "card like" : "card"}" style="background-image: url(${cat.img_link || "/img/cat.jpeg"})">
+    data.forEach(function (cat) {
+        if (cat.id) {
+            let card = `<div class="${cat.favourite ? "card like" : "card"}" style="background-image: url(${cat.img_link || "/project_cats-main/img/cat.jpeg"})">
                 <span>${cat.name}</span>
             </div>`;
             main.innerHTML += card;
@@ -56,20 +58,27 @@ closePopupForm.addEventListener("click", () => {
 
 // создаем экземпляр класса, для формы и добавление котика
 
-const api = new Api("qwaniii");
+let catsData = localStorage.getItem("cats");
+catsData = catsData ? JSON.parse(catsData) : [];
 
-
-const getCats = function(api) {
-    api.getCats()
-        .then(res => res.json())
-        .then(data => {
-            if (data.message === "ok") {
-                updCards(data.data)
-            }
-        })
+const getCats = function (api, store) {
+    if (!store.length) {
+        api.getCats()
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.message === "ok") {
+                    localStorage.setItem("cats", JSON.stringify(data.data));
+                    catsData = [...data.data];
+                    updCards(data.data);
+                }
+            })
+    } else {
+        updCards(store);
+    }
 }
+getCats(api, catsData);
 
-getCats(api);
 
 let form = document.forms[0];
 
@@ -85,10 +94,10 @@ form.img_link.addEventListener("input", (e) => {
 form.addEventListener("submit", e => {
     e.preventDefault();
     let body = {};
-    for (let i = 0; i  < form.elements.length; i++) {
+    for (let i = 0; i < form.elements.length; i++) {
         let inp = form.elements[i];
         if (inp.type === "checkbox") {
-            body[inp.name] = inp.checked;            
+            body[inp.name] = inp.checked;
         } else if (inp.name && inp.value) {
             if (inp.type === "number") {
                 body[inp.name] = +inp.value
@@ -104,14 +113,20 @@ form.addEventListener("submit", e => {
             if (data.message === "ok") {
                 form.reset();
                 closePopupForm.click();
+                api.getCat(body.id)
+                    .then(res => res.json())
+                    .then(cat => {
+                        if (cat.message === "ok") {
+                            catsData.push(cat.data);
+                            localStorage.setItem("cats", JSON.stringify(catsData));
+                            getCats(api, catsData);
+                        } else {
+                            console.log(cat);
+                        }
+                    })
             } else {
                 console.log(data);
+                api.getIds().then(r => r.json()).then(d => console.log(d));
             }
         })
-    
 });
-
-
-
-
-
